59500f822091f51bcf7d90c88428024470975097
Harsh Sharma <harshsharma301a@gmail.com>
2025-04-10 14:49:49 +0530

    Added login logout in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
+import React  from 'react'
 import './App.css'
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appWrite/auth.js'
+import { useEffect } from 'react'
+import {login , logout} from './features/authSlice'
+import {Header , Footer} from './components/index'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    
-    <>
-      <h1>A Blog App with AppWrite</h1>
-    </>
-  )
+  const [loading , setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log(authService.getUser())
+    authService.getUser()
+    .then((userData) => {
+      if(userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  } )
+  
+  
+  return  !loading ? <div className='min-h-screen flex flex-wrap content-between bg-gray-600'>
+    <div className='w-full block text-center'>
+      <Header/>
+      <Footer/>
+    </div>
+  </div> : (null)
 }
 
 export default App
